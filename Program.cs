638cf1e0f57f498dc1d0d98277c299ca4a3ee8e8
Gabriel Lindsay <gabelindsay9@gmail.com>
2023-10-11 10:50:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_5_If_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
